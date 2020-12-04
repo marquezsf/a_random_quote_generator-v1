748bef0f355f815635caa688e56f1b7b171db34d
marquezsf <marquezsf@gmail.com>
2020-12-03 18:47:59 -0800

    added qutoes
    I added an array of quotes with citation
    i also tried to add the funtion to call each quote but still needs alot of work

--- js/script.js
+++ js/script.js
@@ -10,15 +10,28 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+var quotes = [
+  ["You can’t separate peace from freedom because no one can be at peace unless he has his freedom.", MalcomeX],
+  ["Whatever the mind of man can conceive and believe, it can achieve"., Napoleon Hill],
+  ["Strive not to be a success, but rather to be of value.", Albert Einstein],
+  [" I attribute my success to this: I never gave or took any excuse." Florence Nightingale],
+  ["You miss 100% of the shots you don’t take." Wayne Gretzky],
+];
+console.log (quotes);
 
 
 /***
  * `getRandomQuote` function
 ***/
-
-
-
+function getRanndomQuote (arr){
+  let items = ''
+  for (let i = 0, i < arr.lenght, i++) {
+    items += `<li>${arr[i][0]}</li>;
+  }
+  console.log(arr)
+};
+ document.queryselector('main').innerHTML=`
+ 
 /***
  * `printQuote` function
 ***/
